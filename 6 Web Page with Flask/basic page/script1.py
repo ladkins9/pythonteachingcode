1be2e4aa805584d3bacdfb472db01382c0acf992
Laquisha Adkins <ladkins9@students.kennesaw.edu>
2020-10-08 19:10:00 -0400

    Demo

--- 6 Web Page with Flask/basic page/script1.py
+++ 6 Web Page with Flask/basic page/script1.py
@@ -16,5 +16,9 @@ def home():
 def about():
     return render_template("about.html")
 
+    @app.route('/laquisha/')
+    def laquisha():
+        return render_template("adkins.html")
+
 if __name__=="__main__":
     app.run(debug=True)
